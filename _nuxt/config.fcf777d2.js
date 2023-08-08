d8ccc3b7c10c7498e7db25287adb8446cbbe5836
aysegulk7 <141758079+aysegulk7@users.noreply.github.com>
2023-08-08 20:18:02 +0300

    Add files via upload

---
+++ _nuxt/config.fcf777d2.js
@@ -0,0 +1,1 @@
+import{reactive as n}from"vue";import{e as o,d as i}from"./entry.3a8ea910.js";const f={},t=o(f);function r(){const p=i();return p._appConfig||(p._appConfig=n(t)),p._appConfig}export{r as u};
